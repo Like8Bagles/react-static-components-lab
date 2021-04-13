ee4791dad7c7a2de510df10b9a657a6159b76c6a
Anthony <anthonykmbanks@gmail.com>
2021-04-13 16:49:20 -0400

    commit tree e41332fa749b28e4293141cd19ed8ab0bca5a304

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
 	render() {
 		// your code in the return statement below!
@@ -7,7 +8,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<hoverEffect />
 			</div>
 		);
 	}
